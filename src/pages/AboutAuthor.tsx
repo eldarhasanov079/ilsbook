6903ab9e40ab41a3f4d5508229948c6b13341968
Eldar Hasanov <eldarh079@berkeley.edu>
2026-01-18 18:13:41 +0000

    google scholar

--- src/pages/AboutAuthor.tsx
+++ src/pages/AboutAuthor.tsx
@@ -18,6 +18,18 @@ export default function AboutAuthor() {
             Associate Professor<br />
             Azerbaijani University of Languages
           </p>
+          <a 
+            href="https://scholar.google.com/citations?user=OIcH4IgAAAAJ&hl=ru" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="scholar-link"
+            aria-label="View author's Google Scholar profile"
+          >
+            <svg className="scholar-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z" fill="#4285F4"/>
+            </svg>
+            <span>Google Scholar</span>
+          </a>
         </div>
       </div>
     </div>
